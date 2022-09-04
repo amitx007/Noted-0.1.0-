c8e4660ba050a2ed260d4c585961ea2566e4ff69
AMIT <amitmohapatra02@gmail.com>
2022-09-04 13:58:24 +0530

    background js modified

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  chrome.storage.sync.clear();
+  // chrome.storage.sync.clear();
   if (tab.url && tab.url.includes("youtube.com/watch")) {
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
